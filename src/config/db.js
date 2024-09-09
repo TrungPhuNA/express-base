bc6b5506c978eeb85fb06b1adc7d8f825838c47d
phudev <codethue94@gmail.com>
2024-09-09 09:42:25 +0700

    set db url

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/2024_mern_pet', {
+        await mongoose.connect('mongodb+srv://thienhn2k2:0TJ7poYzpcIUTHCX@cluster0.zdwdd.mongodb.net/peca?retryWrites=true&w=majority&appName=Cluster0', {
+        // await mongoose.connect('mongodb://localhost:27017/2024_mern_pet', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
