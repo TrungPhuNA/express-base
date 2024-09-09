d8161e1d25f9d74fccc080fb6fee53f1a5b04a15
phudev <codethue94@gmail.com>
2024-09-09 13:15:30 +0700

    add alert login

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://thienhn2k2:0TJ7poYzpcIUTHCX@cluster0.zdwdd.mongodb.net/peca?retryWrites=true&w=majority&appName=Cluster0', {
-        // await mongoose.connect('mongodb://localhost:27017/2024_mern_pet', {
+        await mongoose.connect(process.env.URL_DATABASE, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
